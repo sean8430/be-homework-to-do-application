9da181e5cb3ef1a7730536dfda30863bda7aa5d2
sean8430 <sean8430@gmail.com>
2022-10-21 22:56:22 +0900

    메서드 이름 수정

--- src/main/java/com/codestates/unit02TodoApplication/controller/TodoAppController.java
+++ src/main/java/com/codestates/unit02TodoApplication/controller/TodoAppController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TodoAppController {
 
     @GetMapping("/")
-    public String helloWorld() {
+    public String printString() {
         return "To-do Application !";
     }
 }
